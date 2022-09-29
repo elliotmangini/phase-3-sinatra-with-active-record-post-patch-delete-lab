92b95e5744d51780f6a96c6d7544fbed40ebef4f
Elliot Mangini <elliot.mangini@gmail.com>
2022-09-29 11:59:18 -0600

    got eem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,28 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id],
+    ).to_json
+  end
+
+  patch '/bakeries/:id' do
+    upd = Bakery.find(params[:id])
+    upd.update(name: params[:name])
+    upd.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    BakedGood.find(params[:id]).destroy.to_json
+  end
 end
+
+
+# t.string "name"
+# t.integer "price"
+# t.integer "bakery_id"
+# t.datetime "created_at", precision: 6, null: false
+# t.datetime "updated_at", precision: 6, null: false
